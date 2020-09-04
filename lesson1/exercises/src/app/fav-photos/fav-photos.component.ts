6eb0008c2af32f8c646deef2db17348736d76731
Queue <elbertq2@gmail.com>
2020-09-04 13:00:32 -0500

    made exercise changes

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'These are my bomb photooooooos!';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://pocket-syndicated-images.s3.amazonaws.com/5eb9824492d28.jpeg';
+  image3 = 'https://pocket-syndicated-images.s3.amazonaws.com/5eb982be55470.jpg';
 
   constructor() { }
 
--- lesson1/exercises/src/app/movie-list/movie-list.component.ts
+++ lesson1/exercises/src/app/movie-list/movie-list.component.ts
@@ -6,11 +6,10 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
-   movies = ['The Manchurian Candidate', 'Oceans 8'];
+   movies = ['Laser Tag: Da Movie','The Manchurian Candidate', 'Oceans 8', 'Galaxy Quest'];
 
    constructor() { }
 
    ngOnInit() {
    }
-
 }
