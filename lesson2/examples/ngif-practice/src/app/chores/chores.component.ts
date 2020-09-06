473d72e0078fd6d3a3dca6d96a54faeb7a88fc3e
Queue <elbertq2@gmail.com>
2020-09-05 21:12:47 -0500

    changes made per textbook

--- lesson2/examples/ngif-practice/src/app/chores/chores.component.ts
+++ lesson2/examples/ngif-practice/src/app/chores/chores.component.ts
@@ -6,8 +6,10 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./chores.component.css']
 })
 export class ChoresComponent implements OnInit {
-   chores = ['Empty dishwasher', 'Complete LaunchCode prep work', 'Buy groceries'];
-   finishedChores = [];
+   // chores = ['Empty dishwasher', 'Complete LaunchCode prep work', 'Buy groceries'];
+   chores = ["Chore1"];
+   finishedChores = ["Left a dollar under the pillow", "Cleaned my wings", "Brushed all of my teeth (that I collected", "Nother Chore1", "Nother Chore2", "Nother Chore3"];
+   // finishedChores = ["Chore2", "Chore3"]
 
    targetImage = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
 
